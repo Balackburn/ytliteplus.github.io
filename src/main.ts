a03b9f4e652e3c1586d081ea8a3b68a89299600b
Mukul Kedia <71291707+Mukul1127@users.noreply.github.com>
2024-09-28 21:57:23 -0400

    final touches (hopefully last time)

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,12 @@ document.getElementById("downloadIpa")!.addEventListener("click", async () => {
   const json = await response.json();
 
   const match = json.body.match(/### Catbox\s*`(.+?\.ipa)`/);
-  if (!match && match[1]) {
+  if (match && match[1]) {
     const fileName = match[1];
     const downloadUrl = `https://files.catbox.moe/${fileName}`;
     window.location.href = downloadUrl;
   } else {
-    document.getElementById("alert")!.classList.remove("hidden");
+    document.getElementById("alert")!.showModal();
   }
 });
 
